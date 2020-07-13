09d603f70314beafa305c9a340f16933ffe0462b
yuyaprograming <ienjoyschoollife@gmail.com>
2020-07-13 20:56:43 +0900

    Second Activityなど

--- app/src/main/java/com/example/calcapp/SecondActivity.kt
+++ app/src/main/java/com/example/calcapp/SecondActivity.kt
@@ -2,11 +2,17 @@ package com.example.calcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val value1 = intent.getIntExtra("VALUE1", 0)
+        val value2 = intent.getIntExtra("VALUE2", 0)
+
+        textView.text = "${value1 + value2}"
     }
 }
