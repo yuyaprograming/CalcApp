3adfc478c385dd8161519ef32da271b0db3a7cb1
yuyaprograming <ienjoyschoollife@gmail.com>
2020-07-14 22:37:35 +0900

    build.grade変更とimport com.google.android.material.snackbar.Snackbarを追加

--- app/src/main/java/com/example/calcapp/MainActivity.kt
+++ app/src/main/java/com/example/calcapp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.calcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button1.setOnClickListener {
-            // EditTextの文字列をTextViewに設定
-            textView.text = editText.text.toString()
-        }
-
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("VALUE1", 10)
+        intent.putExtra("VALUE2", 20)
+        startActivity(intent)
+    }
+
 }
